e8d092291ed777827f713034ae895b4c149f428c
Paul <pengchipaul@gmail.com>
2020-04-03 10:16:56 +1100

    modify index page of react app page

--- resources/js/components/App/App.js
+++ resources/js/components/App/App.js
@@ -4,7 +4,16 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <div className="text-center">App Page</div>
+        <div className="text-center">
+          <h3>
+            This is a react component page
+          </h3>
+        </div>
+        <div className="text-center">
+          <p>
+            Please edit '/resources/js/components/App.js' to start modify this page.
+          </p>
+        </div>
       </div>
     </div>
   );
